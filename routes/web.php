84682e876c0689123a3e29946520583a2170acaf
anildalar <dollorinfotech@gmail.com>
2024-05-22 10:19:15 +0530

    component

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome'); // resourcese/view/welcome.blade.php
 });
+
+Route::get('/another', function () {
+    return view('welcome2'); // resourcese/view/welcome2.blade.php
+});
+
+
 Route::get('/Crucial-T700-Gen5-NVMe-CT1000T700SSD3', function () {
     return view('detail'); // resourcese/view/detail.blade.php
 });
